5101c36dcfaa48e51a2301fa9bf6be23c3fbda30
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-25 11:30:00 +0530

    Initial Commit

---
+++ src/webparts/bwPoc/BwPocWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './BwPocWebPart.module.scss';
+import * as strings from 'BwPocWebPartStrings';
+
+export interface IBwPocWebPartProps {
+  description: string;
+}
+
+export default class BwPocWebPart extends BaseClientSideWebPart<IBwPocWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.bwPoc }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/bwPoc/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBwPocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BwPocWebPartStrings' {
+  const strings: IBwPocWebPartStrings;
+  export = strings;
+}
